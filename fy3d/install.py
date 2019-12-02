3f4d2979b157a7aef90405a462026cc2a65d160e
NingAnMe <ninganme@qq.com>
2019-12-02 11:39:46 +0800

    调整imapp为一个模块

--- fy3d/install.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
-# @Time    : 2019/11/11 16:16
-# @Author  : NingAnMe <ninganme@qq.com>
-
-
-from lib.aid_get_modis_nevi_metadata import get_metadata
-file_ = get_metadata()
-print('Install: {}'.format(file_))
--- fy3d/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
-# @Time    : 2019/11/11 16:01
-# @Author  : NingAnMe <ninganme@qq.com>
