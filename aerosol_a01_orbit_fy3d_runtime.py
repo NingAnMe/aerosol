1b6843d0fc859bdde877f14b0d284299e1f5544a
NingAnMe <ninganme@qq.com>
2021-03-19 16:49:44 +0800

    实时业务

--- aerosol_a01_orbit_fy3d_runtime.py
+++ aerosol_a01_orbit_fy3d_runtime.py
@@ -1,43 +1,117 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time    : 2021-02-03 9:48
 # @Author  : NingAnMe <ninganme@qq.com>
 import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import argparse
+
+import numpy as np
+import pickledb
+
 from aerosol_a01_orbit_fy3d import aerosol_orbit
 from lib.load_mersi import ReadMersiL1
 
-FY3D_L1_PATH = ''
-FY3D_GEO_PATH = ''
-FY3D_CLOUD_PATH = ''
-FY3D_AOD_PATH = ''
+FY3D_L1_PATH = '/home/aodo3/CIMISS_DATA/fy3d/L1'
+FY3D_GEO_PATH = '/home/aodo3/CIMISS_DATA/fy3d/L1'
+FY3D_CLOUD_PATH = '/home/aodo3/CIMISS_DATA/fy3d/L2'
+FY3D_AOD_PATH = '/home/aodo3/FY3D_AEROSOL_DATA/FY3D_MERSI_1KM'
 
-TMP_PATH = 'TMP_DIR'
+TMP_PATH = '/home/aodo3/FY3D_AEROSOL_DATA/TMP'
 
+LAT_RANGE = (17, 54)
+LON_RANGE = (73, 136)
 
-def get_l1_files(l1_dir, dt_now):
-    l1_files = list()
-    return l1_files
+NOT_CHINA_CACHE = 'db.db'
 
-
-def get_geo_file(l1_file):
-    return geo_file
-
-
-def get_cloud_file(l1_file):
-    return cloud_file
+# Z_SATE_C_BAWX_20210317025647_P_FY3D_MERSI_GBAL_L1_20210317_0240_1000M_MS.HDF
+# Z_SATE_C_BAWX_20210317025648_P_FY3D_MERSI_GBAL_L1_20210317_0240_GEO1K_MS.HDF
+# Z_SATE_C_BAWX_20210317004425_P_FY3D_MERSI_ORBT_L2_CLM_MLT_NUL_20210317_0000_1000M_MS.HDF
 
 
-def one_day(dt):
+db = pickledb.load(NOT_CHINA_CACHE, False)
+
+
+def get_files(dt_now: datetime, data_path: str, key_word: str):
+    files = dict()
+    dt_yes = dt_now - relativedelta(days=1)
+    for dt in (dt_now, dt_yes):
+        ymd = dt.strftime("%Y%m%d")
+        y = ymd[:4]
+        m = ymd[4:6]
+        d = ymd[6:8]
+        path_dt = os.path.join(data_path, y, m, d)
+        for filename in os.listdir(path_dt):
+            hm = filename.split('_')[-3]
+            ymdhm = ymd + hm
+            if key_word in filename:
+                files[ymdhm] = os.path.join(path_dt, filename)
+    return files
+
+
+def check_china(l1_file: str, geo_file: str):
+    """
+    检查数据是否经过中国区
+    """
+    rd = ReadMersiL1(l1_file, geo_file=geo_file)
+    lons = rd.get_longitude()
+    lats = rd.get_latitude()
+    china = np.logical_and.reduce(
+        lats > LAT_RANGE[0], lats < LAT_RANGE[1],
+        lons > LON_RANGE[0], lons < LON_RANGE[1])
+    if not np.any(china):
+        return False
+
+
+def get_l1_geo_cloud(dt_now: datetime):
+    l1_files = get_files(dt_now, FY3D_L1_PATH, '1000M_MS')
+    geo_files = get_files(dt_now, FY3D_GEO_PATH, 'GEO1K_MS')
+    cloud_files = get_files(dt_now, FY3D_CLOUD_PATH, '1000M_MS')
+    for ymdhm in l1_files.keys():
+        if db.get(ymdhm) == 'notchina':  # 检测是否经过中国区
+            continue
+        if ymdhm not in geo_files or ymdhm not in cloud_files:  # 检测三个源文件是否同时存在
+            print(f'Warning: {ymdhm} 这个时刻，1000m geo cloud 文件不同时存在')
+            continue
+        l1_file = l1_files[ymdhm]
+        geo_file = geo_files[ymdhm]
+        cloud_file = cloud_file[ymdhm]
+        if not check_china(l1_file, geo_file):  # 检测是否经过中国区
+            db.put(ymdhm, 'notchina')
+            continue
+        else:
+            yield l1_file, geo_file, cloud_file, ymdhm
+
+def one_day(dt: datetime):
     satellite = 'FY3D'
     sensor = 'MERSI'
-    l1_files = get_l1_files(FY3D_L1_PATH, dt_now=dt)
-    for l1_1000m in l1_files:
-        l1_geo = get_geo_file(l1_1000m)
-        l1_cloudmask = get_cloud_file(l1_1000m)
-        l1_reader = ReadMersiL1(l1_1000m)
-        ymdhms = l1_reader.ymd + l1_reader.hms
+    for l1_1000m, l1_geo, l1_cloudmask, ymdhm in get_l1_geo_cloud(dt):
         dir_temp = TMP_PATH
         out_dir = os.path.join(FY3D_AOD_PATH, 'Orbit', 'ymd')
-        aerosol_orbit(l1_1000m, l1_cloudmask, l1_geo, ymdhms, dir_temp, out_dir, satellite, sensor, rewrite=False)
+        aerosol_orbit(l1_1000m, l1_cloudmask, l1_geo, ymdhm + '00',
+                      dir_temp, out_dir, satellite, sensor, rewrite=False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a segmentor')
+    parser.add_argument('--date-start', help='start date  YYYYMMDD')
+
+
+def main():
+    args = parse_args()
+    if args.date_start is not None:
+        ymd = args.date_start
+        y = ymd[:4]
+        m = ymd[4:6]
+        d = ymd[6:8]
+        dt = datetime(y, m , d)
+        one_day(dt)
+    else:
+        dt = datetime.now()
+        one_day(dt)
+
+
+if __name__ == '__main__':
+    main()
+    
