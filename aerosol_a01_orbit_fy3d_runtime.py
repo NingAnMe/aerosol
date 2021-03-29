01d1c448070c6722fb2540cb261cc76178564e7f
NingAnMe <ninganme@qq.com>
2021-03-29 14:23:10 +0800

    查找不反演的错误

--- aerosol_a01_orbit_fy3d_runtime.py
+++ aerosol_a01_orbit_fy3d_runtime.py
@@ -58,15 +58,15 @@ def check_china(l1_file: str, geo_file: str, ymdhm: str):
     """
     cache = db.get(ymdhm + 'china')
     if cache:
-        print(cache)
         return cache
     rd = ReadMersiL1(l1_file, geo_file=geo_file)
     lons = rd.get_longitude()
     lats = rd.get_latitude()
     china = np.logical_and.reduce((lats > LAT_RANGE[0], lats < LAT_RANGE[1],
                                    lons > LON_RANGE[0], lons < LON_RANGE[1]))
-    print(f"{l1_file} {np.sum(china)}")
-    if not np.sum(china) == 0:
+    china_sum = np.sum(china)
+    print(f"{l1_file} {china_sum}")
+    if china_sum == 0:
         db.set(ymdhm + 'china', 'False')
         db.dump()
         return 'False'
